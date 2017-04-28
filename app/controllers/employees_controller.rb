d7091aedb9b7cff119f3a45e0908b14dd4690e4f
Preethi <preethi.nagarajan@nationstarmail.com>
2017-04-28 12:14:06 +0530

    Tech: Preethi/Selva: Added pagination for list of employees used kaminari:bootstrap3

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,7 +4,7 @@ class EmployeesController < ApplicationController
   # GET /employees
   # GET /employees.json
   def index
-    @employees = Employee.all
+    @employees = Employee.order(:first_name).page params[:page]
   end
 
   # GET /employees/1
