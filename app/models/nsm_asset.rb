d96b0d21ff6b0745f08b203e6330f938fd23c074
Selva Chezhian <selvachezhian.l@nationstarmail.com>
2017-05-01 15:03:46 +0530

    TECH: Chezhian: report page for assets

--- app/models/nsm_asset.rb
+++ app/models/nsm_asset.rb
@@ -2,6 +2,7 @@ class NsmAsset < ApplicationRecord
   has_one :employee_asset
   belongs_to :nsm_asset_type
 
+  scope :assigned, -> { where(assigned: true) }
   scope :un_assigned, -> { where(assigned: [false, nil]) }
   scope :by_code, ->(code) { where('code LIKE ?', "%#{code}%") }
 
--- app/models/nsm_asset_type.rb
+++ app/models/nsm_asset_type.rb
@@ -1,3 +1,27 @@
 class NsmAssetType < ApplicationRecord
   has_many :nsm_assets
+
+  def purchased
+    nsm_assets
+  end
+
+  def purchased_count
+    purchased.count
+  end
+
+  def assigned
+    nsm_assets.assigned
+  end
+
+  def assigned_count
+    assigned.count
+  end
+
+  def available
+    nsm_assets.un_assigned
+  end
+
+  def available_count
+    available.count
+  end
 end
